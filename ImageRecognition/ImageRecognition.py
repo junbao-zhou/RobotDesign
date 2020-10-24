90c000098e7e3cf069deeafde80dc08ada1fb958
bowmanchow <bowmanchow@gmail.com>
2020-10-24 12:01:12 +0800

    add image recognition

---
+++ ImageRecognition/ImageRecognition.py
@@ -0,0 +1,9 @@
+# recognize yellow duck and pikaqiu
+import matplotlib.pyplot as plt
+import matplotlib.image as pli
+
+
+image = plt.imread(
+    "/diskd/MyData/Embedded/智能机器人设计/RobotDesign/ImageRecognition/trainingset_image/109816386.jpg")
+
+plt.imshow(image)
